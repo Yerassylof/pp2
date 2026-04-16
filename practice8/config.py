4b7e68856f380b63c5bc4eafd2dcaa1b14c70ef3
Yerassylof <er_murat@kbtu.kz>
2026-04-16 22:55:38 +0500

    Delete practice8 directory

--- practice8/config.py
+++
@@ -1,8 +0,0 @@
-def load_config():
-    return {
-        "host": "localhost",
-        "database": "your_db_name",
-        "user": "postgres",
-        "password": "your_password",
-        "port": 5432
-    }
